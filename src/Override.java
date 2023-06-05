ea0550413c53ac9011ca3ca95cdb61425d5d564c
Michael Coyle <michael.david.coyle4@gmail.com>
2023-06-05 13:13:54 -0500

    add notes

--- src/Override.java
+++ src/Override.java
@@ -1,3 +1,7 @@
+// method overriding
+// allows a subclass or child class to provide a specific implementation of a method that is already
+// provided by one of its superclasses or parent classes
+
 public class Override {
     public static void main(String[] args) {
         Poppable poppable = new Poppable();
