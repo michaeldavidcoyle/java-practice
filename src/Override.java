4bec3f1c6b0c52e5de848d4be0620949aab0b612
Michael Coyle <michael.david.coyle4@gmail.com>
2023-06-05 13:04:43 -0500

    examples and notes on method overriding

---
+++ src/Override.java
@@ -0,0 +1,41 @@
+public class Override {
+    public static void main(String[] args) {
+        Poppable poppable = new Poppable();
+        poppable.pop(); // pop
+
+        Poppable balloon = new Balloon();
+        Balloon balloon2 = new Balloon();
+        balloon.pop(); // POP!
+        balloon2.pop(4);
+
+        Poppable bubble = new Bubble();
+        bubble.pop(); // blup
+    }
+}
+
+class Poppable {
+    public void pop() {
+        System.out.println("pop");
+    }
+}
+
+class Balloon extends Poppable {
+    // method overriding: same signature, method name and parameters, as superclass
+    public void pop() {
+        System.out.println("POP!");
+    }
+
+    public void pop(int n) { // NOT method overriding, signature does not match. Rather, method overloading
+        for (int i = 0; i < n; i++) {
+            System.out.print("pop ");
+        }
+        System.out.println();
+    }
+}
+
+class Bubble extends Poppable {
+    // method overriding: same signature, method name and parameters, as superclass
+    public void pop() {
+        System.out.println("blup");
+    }
+}
