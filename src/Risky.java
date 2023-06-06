0f193c0b35d76646495510f6e530560a478733e7
Michael Coyle <michael.david.coyle4@gmail.com>
2023-06-06 00:22:56 -0500

    examples w/ notes on exceptions

---
+++ src/Risky.java
@@ -0,0 +1,36 @@
+// exceptions
+//     * reading a corrupted file
+//     * network connection failure
+//     * division by zero
+//     * calling method without an object
+//     * exception types:
+//         + IOException
+//         + ConnectionException
+//         + ArithmeticException
+//         + NullPointerException
+
+//     ------Throwable-------
+//    |                      |
+//  Error                Exception --> checked
+//                           |       (must be handled or code won't compile)
+//                           |
+//                    RuntimeException
+//                           |
+//                        unchecked (occurs @ runtime)
+
+import java.io.FileInputStream;
+
+public class Risky {
+    public static void main(String[] args) {
+        // unchecked exceptions - will still compile
+        int x = 1 / 0; // ArithmeticException
+        Object object = null;
+        object.toString(); // NullPointerException
+        int[] intArray = new int[5];
+        intArray[10] = 10; // ArrayIndexOutOfBoundsException
+
+        // checked exception - will NOT compile
+        // Unhandled exception: java.io.FileNotFoundException
+        FileInputStream file = new FileInputStream("file.txt");
+    }
+}
