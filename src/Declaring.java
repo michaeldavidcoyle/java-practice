43a5ec8d24ea9c7b2f45d2f8d62c8f77ce0a48d9
Michael Coyle <michael.david.coyle4@gmail.com>
2023-06-06 00:45:33 -0500

    examples w/ notes on throws declaration

---
+++ src/Declaring.java
@@ -0,0 +1,17 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class Declaring {
+    public static void main(String[] args) {
+        try {
+            read();
+        } catch (FileNotFoundException fileNotFoundException) {
+            System.out.println("You don't need to see his identification. This is not the file you're looking for.");
+        }
+    }
+
+    // declaring the exception
+    static void read() throws FileNotFoundException { // passes handling of exception up the chain
+        FileInputStream file = new FileInputStream("file.text");
+    }
+}
