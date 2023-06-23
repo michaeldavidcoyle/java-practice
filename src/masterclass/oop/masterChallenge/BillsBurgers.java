09755b53b8686c9c1cfe28d27c2a7987e8c60c94
Michael Coyle <michael.david.coyle4@gmail.com>
2023-06-22 22:59:13 -0500

    replace single toppings field with instance variables for individual toppings. refactor customize method. add toppings method. add addCharge method. refactor total and printOrder

--- src/masterclass/oop/masterChallenge/BillsBurgers.java
+++ src/masterclass/oop/masterChallenge/BillsBurgers.java
@@ -2,7 +2,7 @@ package masterclass.oop.masterChallenge;
 
 public class BillsBurgers {
     public static void main(String[] args) {
-        Burger burger = new Burger("Single", 7.95);
+        Burger burger = new Burger("Single Burger", 7.95);
         burger.printOrder();
     }
 }
--- src/masterclass/oop/masterChallenge/Burger.java
+++ src/masterclass/oop/masterChallenge/Burger.java
@@ -3,36 +3,87 @@ package masterclass.oop.masterChallenge;
 public class Burger {
     private String type;
     private double basePrice;
-    private StringBuilder toppings;
+    private int extraPatties;
+    private int cheeseSlices;
+    private int baconSlices;
+    private int avocadoPortions;
+    private int jalepenoPortions;
+    private String mayo = "mayo";
+    private String mustard = "mustard";
+    private String lettuce = "lettuce";
+    private String tomato = "tomato";
+    private String onion = "onion";
+    private String pickle = "";
+    private String customTopping = "";
 
-    protected final static double costPerExtraPatty = 2.5;
-    protected final static double costPerCheeseSlice = 0.5;
-    protected final static double costPerBacon = 0.75;
+    protected static double costPerExtraPatty = 2.75;
+    protected static double costPerCheeseSlice = 0.65;
+    protected static double costPerBacon = 0.90;
+    protected static double costPerAvocado = 1.75;
+    protected static double costPerJalepeno = 0.75;
 
     public Burger(String type, double basePrice) {
         this.type = type;
         this.basePrice = basePrice;
-        this.toppings = new StringBuilder("mayo, mustard, lettuce, tomato, onion");
     }
 
     public void customize(String item, String level) {
-        int itemIndex = toppings.indexOf(item);
-        if (itemIndex < 0) {
-            toppings.append(String.format(", %s %s", level, item));
-            return;
+        boolean extra = level.equals("extra");
+        switch (item) {
+            case "patty" -> extraPatties = extra ? 2 : 1;
+            case "cheese" -> cheeseSlices = extra ? 2 : 1;
+            case "bacon" -> baconSlices = extra ? 6 : 3;
+            case "avocado" -> avocadoPortions = extra ? 2 : 1;
+            case "jalepeno" -> jalepenoPortions = extra ? 2 : 1;
+            case "mayo" -> mayo = extra ? "extra mayo" : "";
+            case "mustard" -> mustard = extra ? "extra mustard" : "";
+            case "lettuce" -> lettuce = extra ? "extra lettuce" : "";
+            case "tomato" -> tomato = extra ? "extra tomato" : "";
+            case "onion" -> onion = extra ? "extra onion" : "";
+            case "pickle" -> pickle = extra ? "extra pickle" : "pickle";
+            default -> customTopping = extra ? ("extra " + item) : item;
         }
-        toppings.insert(toppings.indexOf(item) - 1, level + " ");
     }
 
-    public double calculateTotal(int extraPatties, int cheeseSlices, int baconSlices) {
-        double extras = (costPerExtraPatty * extraPatties) +
-                (costPerCheeseSlice * cheeseSlices) +
-                (costPerBacon * baconSlices);
+    public String toppings() {
+        String patty = extraPatties > 0 ? String.format("%dx patty.....$%.2f%n", extraPatties, addCharge("patty") * extraPatties) : "";
+        String cheese = cheeseSlices > 0 ? String.format("%dx cheese.....$%.2f%n", cheeseSlices, addCharge("cheese") * cheeseSlices) : "";
+        String bacon = baconSlices > 0 ? String.format("%dx bacon.....$%.2f%n", baconSlices, addCharge("bacon") * baconSlices) : "";
+        String avocado = avocadoPortions > 0 ? String.format("%dx avocado.....%.2f%n", avocadoPortions, addCharge("avocado") * avocadoPortions) : "";
+        String jalepeno = jalepenoPortions > 0 ? String.format("%dx jalepeno.....%.2f%n", jalepenoPortions, addCharge("jalepeno") * jalepenoPortions) : "";
+        String mayo = this.mayo.isEmpty() ? "" : String.format("%s.....$0.00%n", this.mayo);
+        String mustard = this.mustard.isEmpty() ? "" : String.format("%s.....$0.00%n", this.mustard);
+        String lettuce = this.lettuce.isEmpty() ? "" : String.format("%s.....$0.00%n", this.lettuce);
+        String tomato = this.tomato.isEmpty() ? "" : String.format("%s.....$0.00%n", this.tomato);
+        String onion = this.onion.isEmpty() ? "" : String.format("%s.....$0.00%n", this.onion);
+        String pickle = this.pickle.isEmpty() ? "" : String.format("%s.....$0.00%n", this.pickle);
+        String custom = this.customTopping.isEmpty() ? "" : String.format("%s.....$0.00%n", this.customTopping);
+
+        return patty + cheese + bacon + avocado + jalepeno + mayo + mustard + lettuce + tomato + onion + pickle + custom;
+    }
+
+    public double total() {
+        double extras = addCharge("patty") +
+                addCharge("cheese") +
+                addCharge("bacon") +
+                addCharge("avocado") +
+                addCharge("jalepeno");
         return basePrice + extras;
     }
 
+    private double addCharge(String item) {
+        return switch (item) {
+            case "patty" -> costPerExtraPatty * extraPatties;
+            case "cheese" -> costPerCheeseSlice * cheeseSlices;
+            case "bacon" -> costPerBacon * baconSlices;
+            case "avocado" -> costPerAvocado * avocadoPortions;
+            case "jalepeno" -> costPerJalepeno * jalepenoPortions;
+            default -> 0d;
+        };
+    }
+
     public void printOrder() {
-        System.out.printf("%s with %s: $%.2f%n", type, toppings, basePrice);
-        System.out.printf("Total: $%.2f%n", calculateTotal(0, 0, 0));
+        System.out.printf("%s.....$%.2f%n%s%n", type, basePrice, toppings());
+        System.out.printf("Total: $%.2f%n", total());
     }
 }
