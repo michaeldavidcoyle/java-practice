43921c2bfde07371f6eb500ceb1dde3c95022bba
Michael Coyle <michael.david.coyle4@gmail.com>
2023-06-23 09:57:57 -0500

    add classes for Side and Drink

--- src/masterclass/oop/masterChallenge/Drink.java
+++ src/masterclass/oop/masterChallenge/Drink.java
@@ -1,4 +1,41 @@
 package masterclass.oop.masterChallenge;
 
 public class Drink {
+    private String type;
+    private String size;
+    private double price;
+    private boolean refillable;
+
+    public Drink(String type, String size, double price) {
+        this.type = type;
+        this.size = size;
+        this.price = price;
+        refillable = type.equals("tea") || type.equals("soda");
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        if ( !(size.equals("small") || size.equals("medium") || size.equals("large")) ) return;
+        this.size = size;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        if (price < 0) return;
+        this.price = price;
+    }
 }
--- src/masterclass/oop/masterChallenge/Side.java
+++ src/masterclass/oop/masterChallenge/Side.java
@@ -1,4 +1,39 @@
 package masterclass.oop.masterChallenge;
 
 public class Side {
+    private double price;
+    private String size;
+    private String type;
+
+    public Side(double price, String size, String type) {
+        this.price = price;
+        this.size = size;
+        this.type = type;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        if ( !(size.equals("small") || size.equals("medium") || size.equals("large")) ) return;
+        this.size = size;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        if (price < 0) return;
+        this.price = price;
+    }
 }
