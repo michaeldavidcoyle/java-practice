66a7760473e53b7a54124406c3df4b6d86881ccf
Michael Coyle <michael.david.coyle4@gmail.com>
2023-07-06 17:39:41 -0500

    create OrderItem class with quantity and ProductForSale

---
+++ src/masterclass/abstraction/OrderItem.java
@@ -0,0 +1,19 @@
+package masterclass.abstraction;
+
+public class OrderItem {
+    private int quantity;
+    private ProductForSale product;
+
+    public OrderItem(int quantity, ProductForSale product) {
+        this.quantity = quantity;
+        this.product = product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public ProductForSale getProduct() {
+        return product;
+    }
+}
